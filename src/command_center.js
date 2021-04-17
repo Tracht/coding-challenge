d644ee80c4d210e5d50a60f3dcf73485d49f9036
Tracht <marija.trachtenberg@gmail.com>
2021-04-17 16:34:51 +0100

    Documentation for Command Center

--- src/command_center.js
+++ src/command_center.js
@@ -1,4 +1,18 @@
-class CommandCenter {
+/**
+ * Command Center Class.
+ * 
+ * It is initialised with 2 arguments - the x & y coordinates representing the bounds of the ocean.
+ *      A location (x,y coordinates - type = integer).
+ * 
+ * It has a method 'isLost', which takes a 'Ship' instance as an argument.
+ *      It checks if the ship is out of bounds. 
+ *      The ocean bounds start at 0,0. 
+ *      The end of the ocean bounds are a positive combination of x and y, i.e. (5, 3). 
+ *      A ship is out of bounds if x and y are negative. 
+ *      A ship is out of bounds if x and y are larger than the initialised x & y coordinates.
+ */
+
+ class CommandCenter {
     constructor(x,y){
         this.x = x; 
         this.y = y;
