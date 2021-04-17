bfbc260580eca569e10ff7bc7e37fdde53e03b64
Tracht <marija.trachtenberg@gmail.com>
2021-04-17 13:51:02 +0100

    Passing test - ship moves in a circle, return to original position & orientation

--- ship.test.js
+++ ship.test.js
@@ -12,6 +12,27 @@ describe('Ship', () => {
         const ship = new Ship(0, 0, 'N');
         ship.move('F'); 
         expect(ship.position).toEqual(new Position(0, 1, 'N'));
+
+        ship.move('R'); 
+        expect(ship.position).toEqual(new Position(0, 1, 'E'));
+
+        ship.move('F'); 
+        expect(ship.position).toEqual(new Position(1, 1, 'E'));
+
+        ship.move('R'); 
+        expect(ship.position).toEqual(new Position(1, 1, 'S'));
+
+        ship.move('F'); 
+        expect(ship.position).toEqual(new Position(1, 0, 'S'));
+
+        ship.move('R'); 
+        expect(ship.position).toEqual(new Position(1, 0, 'W'));
+
+        ship.move('F'); 
+        expect(ship.position).toEqual(new Position(0, 0, 'W'));
+
+        ship.move('R'); 
+        expect(ship.position).toEqual(new Position(0, 0, 'N'));
     })
 
 
