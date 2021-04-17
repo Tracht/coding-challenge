8d1b36518add9eee664f176029976e13d8f60381
Tracht <marija.trachtenberg@gmail.com>
2021-04-17 16:31:22 +0100

    Documentation for Ship Class

--- src/ship.js
+++ src/ship.js
@@ -1,8 +1,32 @@
-const { Position } = require ('./position');
+/**
+ * Ship Class.
+ * 
+ * It is initialised with the Position Class and 3 arguments:
+ *      A location (x,y coordinates - type = integer) and 
+ *      A starting orientation (N, S, E, W - type = strings)
+ * 
+ * It has a method 'move', which moves the ship to a new position and orientation.
+ *      It relies on the Position Class method 'updatePosition'
+ *      It takes a single argument, 'instruction', (F, L, R - type = string) 
+ *      It returns a new position instance.
+ * 
+ * The 'getX()' method returns the ship's current x-coordinate. 
+ * The 'getY()' method returns the ship's current y-coordinate.
+ * The 'getOrienation()' method returns the ship's current orientation.
+ */
 
+const { Position } = require ('./position');
 class Ship {
     constructor(x, y, orientation) {
         this.position = new Position(x, y, orientation);
+    }
+
+    move(instruction) {
+        this.position = this.position.updatePosition(instruction);
+    }
+
+    position() {
+        return this.position;
     }
 
     getX(){
@@ -15,14 +39,6 @@ class Ship {
 
     getOrientation(){
         return this.position.orientation;
-    }
-
-    position() {
-        return this.position;
-    }
-
-    move(instruction) {
-        this.position = this.position.updatePosition(instruction);
     }
 }
 
