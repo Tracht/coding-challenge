f944e70013bcb32c9bb3796426de71f43df33e0e
Tracht <marija.trachtenberg@gmail.com>
2021-04-17 15:51:20 +0100

    Journey Class - failing test - 3rd ship is lost, it shouldn't be

--- src/journey.test.js
+++ src/journey.test.js
@@ -19,4 +19,12 @@ describe('Ship status', () => {
         expect(journey.report(ship, 'FRRFLLFFRRFLL')).toEqual('3 3 N LOST');
     })
 
+    it('Ship 3 - not lost', () => {
+        const command_center =  new CommandCenter(5,3);
+        const ship = new Ship(0, 3, 'W');
+        const journey = new Journey(command_center);
+
+        expect(journey.report(ship, 'LLFFFLFLFL')).toEqual('2 3 S');
+    })
+
 })
