67258f9ba78ee14a40354cd9548c0f316104b63a
Tracht <marija.trachtenberg@gmail.com>
2021-04-17 14:43:17 +0100

    Creates command_center test, checks if ship of -1,-1 is lost

---
+++ src/command_center.js
@@ -0,0 +1,17 @@
+const ship = require('./ship');
+
+class CommandCenter {
+    constructor(x,y){
+        this.x = x; 
+        this.y = y;
+    }
+
+    isLost(ship){
+        // if it is negatively out of bounds
+        
+    }
+}
+
+module.exports = {
+    CommandCenter
+}
---
+++ src/command_center.test.js
@@ -0,0 +1,12 @@
+const { Ship } = require("./ship");
+const { CommandCenter } = require("./command_center");
+
+describe('Command Center with Grid 5,3', () => {
+
+    it('Lost Ship at -1, -1', () => {
+        var command_center = new CommandCenter(5,3);
+        var ship = new Ship(-1,-1,'N');
+        expect(command_center.isLost(ship)).toEqual(true);
+    });
+
+})
