8aaae89c2685bd093e71c73cced303aea1c6c969
Tracht <marija.trachtenberg@gmail.com>
2021-04-17 16:42:11 +0100

    Documentation for Journey Class

--- src/journey.js
+++ src/journey.js
@@ -1,4 +1,23 @@
-class Journey {
+/**
+ * Journey Class.
+ * 
+ * It is initialised with 1 argument - the Command_Center Class instance.
+ * 
+ * It has a method 'report', which takes 2 arguments. 
+ *      First argument: an instance of a Ship
+ *      Second argument: the instructions given to the ship ('RFRFRFRF' - type = string)
+ * 
+ *      This method returns the Ship's status. 
+ *      For ships that become lost, its last position (prior to going over the edge) is reported like so: 
+ *      (I.e. If the ocean bounds are (3,3), and the ship's position is (4,3), then report the status like this:)
+ *              I.e. '3 3 N LOST'
+ * 
+ *      For ships that never got lost, its position is reported like so:
+ *               I.e. '3 3 N'
+ * 
+ */
+
+ class Journey {
     constructor(command_center){
         this.command_center = command_center;
     }
