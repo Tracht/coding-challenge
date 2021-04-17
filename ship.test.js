a7018e4815ab94094ea6933a6cc01c00b0453cf7
Tracht <marija.trachtenberg@gmail.com>
2021-04-17 13:39:45 +0100

    Writes the first Ship Test, checks it can initialise a Position

---
+++ ship.test.js
@@ -0,0 +1,11 @@
+const { Ship } = require('./ship');
+const { Position } = require('./position');
+
+describe('Ship', () => {
+
+    it('Can initialise a ship with its current position & orientation', () => {
+        const ship = new Ship(0, 0, 'N');
+        expect(ship.position).toEqual(new Position(0, 1, 'N'));
+    })
+
+})
